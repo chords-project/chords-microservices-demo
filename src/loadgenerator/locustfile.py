ce0c0180c211eafc30ff0b71f44cc40dd4a5d031
viktorstrate <viktorstrate@gmail.com>
2024-11-26 11:00:37 +0100

    Replace src with upstream

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 import random
-from locust import FastHttpUser, TaskSet, between, constant_pacing, task
+from locust import FastHttpUser, TaskSet, between
 from faker import Faker
 import datetime
 fake = Faker()
@@ -31,36 +31,29 @@ products = [
     'LS4PSXUNUM',
     'OLJCESPC7Z']
 
-
 def index(l):
     l.client.get("/")
-
 
 def setCurrency(l):
     currencies = ['EUR', 'USD', 'JPY', 'CAD', 'GBP', 'TRY']
     l.client.post("/setCurrency",
-                  {'currency_code': random.choice(currencies)})
-
+        {'currency_code': random.choice(currencies)})
 
 def browseProduct(l):
     l.client.get("/product/" + random.choice(products))
 
-
 def viewCart(l):
     l.client.get("/cart")
-
 
 def addToCart(l):
     product = random.choice(products)
     l.client.get("/product/" + product)
     l.client.post("/cart", {
         'product_id': product,
-        'quantity': random.randint(1, 10)})
-
-
+        'quantity': random.randint(1,10)})
+    
 def empty_cart(l):
     l.client.post('/cart/empty')
-
 
 def checkout(l):
     addToCart(l)
@@ -77,64 +70,23 @@ def checkout(l):
         'credit_card_expiration_year': random.randint(current_year, current_year + 70),
         'credit_card_cvv': f"{random.randint(100, 999)}",
     })
-
-
+    
 def logout(l):
-    l.client.get('/logout')
-
-# class UserBehavior(TaskSet):
-
-#     def on_start(self):
-#         index(self)
-
-#     tasks = {
-#         index: 1,
-#         setCurrency: 2,
-#         browseProduct: 10,
-#         addToCart: 2,
-#         viewCart: 3,
-#         checkout: 1
-#     }
+    l.client.get('/logout')  
 
 
-class CheckoutBehaviour(TaskSet):
+class UserBehavior(TaskSet):
 
     def on_start(self):
         index(self)
 
-    @task
-    def checkout_flow(self):
-        setCurrency(self)
-
-        # Add products to cart
-        max_products = random.randrange(2, len(products))
-        buy_products = random.sample(products, max_products)
-        for product in buy_products:
-            self.client.post("/cart", {
-                'product_id': product,
-                'quantity': random.randint(1, 10)})
-
-        # Checkout
-        current_year = datetime.datetime.now().year+1
-        self.client.post("/cart/checkout", {
-            'email': fake.email(),
-            'street_address': fake.street_address(),
-            'zip_code': fake.zipcode(),
-            'city': fake.city(),
-            'state': fake.state_abbr(),
-            'country': fake.country(),
-            'credit_card_number': fake.credit_card_number(card_type="visa"),
-            'credit_card_expiration_month': random.randint(1, 12),
-            'credit_card_expiration_year': random.randint(current_year, current_year + 70),
-            'credit_card_cvv': f"{random.randint(100, 999)}",
-        })
-
+    tasks = {index: 1,
+        setCurrency: 2,
+        browseProduct: 10,
+        addToCart: 2,
+        viewCart: 3,
+        checkout: 1}
 
 class WebsiteUser(FastHttpUser):
-    tasks = [
-        # UserBehavior
-        CheckoutBehaviour
-    ]
-
-    wait_time = constant_pacing(5)
-    # wait_time = between(1, 5)
+    tasks = [UserBehavior]
+    wait_time = between(1, 10)
